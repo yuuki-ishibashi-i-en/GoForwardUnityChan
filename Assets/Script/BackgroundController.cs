f35783461843c370478aec2104174c89520027ae
yuuki-ishibashi-i-en <y-ishibashi@i-enter.co.jp>
2021-04-14 14:49:34 +0900

    課題完了

---
+++ Assets/Script/BackgroundController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundController : MonoBehaviour
+{
+    private float scrollSpeed = -1;
+
+    private float deadLine = -16;
+
+    private float startLine = 15.8f;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(this.scrollSpeed * Time.deltaTime, 0, 0);
+
+        if (transform.position.x < this.deadLine)
+        {
+            transform.position = new Vector2(this.startLine, 0);
+        }
+    }
+}
--- Assets/Script/UnityChanController.cs
+++ Assets/Script/UnityChanController.cs
@@ -6,13 +6,24 @@ public class UnityChanController : MonoBehaviour
 {
     Animator animator;
 
+    Rigidbody2D rigid2D;
+
     private float groundLevel = -3.0f;
+
+    private float dump = 0.8f;
+
+    float jumpVelocity = 20;
+
+    private float deadLine = -9;
 
     // Use this for initialization
     void Start()
     {
         this.animator = GetComponent<Animator>();
+
+        this.rigid2D = GetComponent<Rigidbody2D>();
     }
+
     // Update is called once per frame
     void Update()
     {
@@ -20,5 +31,25 @@ public class UnityChanController : MonoBehaviour
 
         bool isGround = (transform.position.y > this.groundLevel) ? false : true;
         this.animator.SetBool("isGround", isGround);
+
+        if (Input.GetMouseButtonDown(0) && isGround)
+        {
+            this.rigid2D.velocity = new Vector2(0, this.jumpVelocity);
+        }
+
+        if (Input.GetMouseButton(0) == false)
+        {
+            if (this.rigid2D.velocity.y > 0)
+            {
+                this.rigid2D.velocity *= this.dump;
+            }
+        }
+
+        if (transform.position.x < this.deadLine)
+        {
+            GameObject.Find("Canvas").GetComponent<UIController>().GameOver();
+
+            Destroy(gameObject);
+        }
     }
 }
