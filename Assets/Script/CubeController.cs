d243c8aeaef2626b44bc6e021ff19785923c3813
yuuki-ishibashi-i-en <y-ishibashi@i-enter.co.jp>
2021-04-14 11:01:09 +0900

    Initial commit

---
+++ Assets/Script/CubeController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour
+{
+    private float speed = -12;
+
+    private float deadLine = -10;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(this.speed * Time.deltaTime, 0, 0);
+
+        if (transform.position.x < this.deadLine)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Script/UnityChanController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnityChanController : MonoBehaviour
+{
+    Animator animator;
+
+    private float groundLevel = -3.0f;
+
+    // Use this for initialization
+    void Start()
+    {
+        this.animator = GetComponent<Animator>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        this.animator.SetFloat("Horizontal", 1);
+
+        bool isGround = (transform.position.y > this.groundLevel) ? false : true;
+        this.animator.SetBool("isGround", isGround);
+    }
+}
